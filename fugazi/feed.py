687f585b6694771b4c7c858314403e28d5f6a514
George Murdocca <george@linuxdojo.com>
2019-07-25 23:55:08 +1000

    added get_assets_age function

--- fugazi/feed.py
+++ fugazi/feed.py
@@ -24,31 +24,34 @@ with open(config_file, "r") as config_file:
     config = yaml.load(config_file.read(), Loader=yaml.Loader)
 
 
-class Coin:
+class Asset:
 
-    def __init__(self, api_key, symbol, freshness=5):
-        """
-        freshness in seconds
-        """
+    """
+    A class representing an crypto asset, eg. Bitcoin
+    """
+
+    def __init__(self, api_key, symbol, cache_timeout=5):
         self.api_key = api_key
         self.symbol = symbol
+        self.asset_file = "./assets.json"
+        self.cache_timeout = cache_timeout
+        self.asset_detail = None
         self.default_headers = {
                 "Content-Type": "application/json",
                 "X-API-Key": self.api_key
                 }
-        self.asset_file = "./assets.json"
-        self.freshness = freshness
-        self.asset_detail = None
 
     @property
     def asset(self):
-        return self._get_asset_by_symbol(self.symbol)
+        return self.get_asset_by_symbol(self.symbol)
 
-    def _get_all_assets(self):
-        if os.path.exists(self.asset_file):
+    def get_all_assets(self, refresh=False):
+        if not refresh and os.path.exists(self.asset_file):
             with open(self.asset_file, "r") as asset_file:
-                assets = json.load(asset_file)
+                asset_data = json.load(asset_file) 
+                assets = asset_data['assets']
         else:
+            timestamp = time.time()
             assets = []
             skip = 0
             while True:
@@ -66,22 +69,37 @@ class Coin:
                 else:
                     break
             # write assets to file
+            asset_data = {
+                    "timestamp": timestamp,
+                    "assets": assets
+                    }
             with open(self.asset_file, "w") as asset_file:
-                asset_file.write(json.dumps(assets))
+                asset_file.write(json.dumps(asset_data))
         return assets
+
+    def get_assets_age(self):
+        """
+        Returns age in seconds of cached assets or None if not cached
+        """
+        if not os.path.exists(self.asset_file):
+            return None
+        with open(self.asset_file, "r") as asset_file:
+            asset_data = json.load(asset_file) 
+            timestamp = asset_data['timestamp']
+        return time.time() - timestamp
 
     def is_fresh(self):
         """
-        returns True if asset_detail age < self.freshness
+        returns True if asset_detail age < self.cache_timeout
         """
-        if self.asset_detail and time.time() - self.asset_detail['timestamp'] < self.freshness:
+        if self.asset_detail and time.time() - self.asset_detail['timestamp'] < self.cache_timeout:
             return True
         return False
 
-    def _get_asset_by_symbol(self, symbol):
+    def get_asset_by_symbol(self, symbol):
         if not self.is_fresh():
             found = False
-            assets = self._get_all_assets()
+            assets = self.get_all_assets()
             for asset in assets:
                 if 'originalSymbol' in asset and asset['originalSymbol'] == symbol:
                     found = True
@@ -106,13 +124,11 @@ class Coin:
     def get_24hour_volume(self):
         return self.asset['volume']
         
-        
 
 def main():
     apikey = config['apikeys']['cryptoapi']
-    coin = Coin(apikey, 'ETH')
+    coin = Asset(apikey, 'ETH')
     print(coin.get_ath(), coin.get_current_price(), coin.get_24hour_volume()) 
-
 
 
 if __name__ == "__main__":
